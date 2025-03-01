68d613e7b6b2de7eb16d7f72e200ffccfe5cd136
Nealle Page <nealle.page@gmail.com>
2025-03-01 21:24:11 +0000

    new blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
